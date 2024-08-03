bb470e2263f21d0d13279cd33bc382a0a06cde80
Alexandre <eztalenda@gmail.com>
2024-08-03 19:48:50 -0300

    feat: Start project with React and TypeScript setup"

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,16 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import { ThemeProvider } from '@mui/material/styles'
+import Button from '@mui/material/Button'
+import theme from './Themes/theme'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <ThemeProvider theme={theme}>
+      <Button variant="contained" color="primary">
+        Clique Aqui
+      </Button>
+    </ThemeProvider>
+  )
 }
 
-export default App;
+export default App
