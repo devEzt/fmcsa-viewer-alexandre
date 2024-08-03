756e6c738acd79fb2b7f7b58ee97b65185b6d61d
Alexandre <eztalenda@gmail.com>
2024-08-03 20:53:14 -0300

    feat: implements an initial basis for the project theme

--- src/Themes/theme.ts
+++ src/Themes/theme.ts
@@ -1,12 +1,25 @@
 import { createTheme } from '@mui/material/styles'
 
+declare module '@mui/material/styles' {
+  interface Palette {
+    menu?: Palette['primary']
+  }
+  interface PaletteOptions {
+    menu?: PaletteOptions['primary']
+  }
+}
+
 const theme = createTheme({
   palette: {
-    primary: {
-      main: '#556cd6',
+    background: {
+      default: '#282828',
+      paper: '#3c3c3c',
     },
-    secondary: {
-      main: '#19857b',
+    text: {
+      primary: '#FFFFFF',
+    },
+    menu: {
+      main: '#141414',
     },
   },
 })
