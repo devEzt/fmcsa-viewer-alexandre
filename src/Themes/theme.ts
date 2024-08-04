f500997639b3d58d74550e7e74e4662205ee4f56
Alexandre <eztalenda@gmail.com>
2024-08-04 02:46:51 -0300

    feat:change in theme colors to better adapt

--- src/Themes/theme.ts
+++ src/Themes/theme.ts
@@ -3,23 +3,28 @@ import { createTheme } from '@mui/material/styles'
 declare module '@mui/material/styles' {
   interface Palette {
     menu?: Palette['primary']
+    icon?: Palette['primary']
   }
   interface PaletteOptions {
     menu?: PaletteOptions['primary']
+    icon?: PaletteOptions['primary']
   }
 }
 
 const theme = createTheme({
   palette: {
     background: {
-      default: '#282828',
-      paper: '#3c3c3c',
+      default: '#1E2122',
+      paper: '#2D3133',
     },
     text: {
       primary: '#FFFFFF',
     },
     menu: {
-      main: '#141414',
+      main: '#0F1011',
+    },
+    icon: {
+      main: '#282828',
     },
   },
 })
--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,14 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import Routes from './Routes' // Importando o componente de Rotas
-import { ThemeProvider } from '@mui/material/styles'
+import Routes from './Routes'
+import { ThemeProvider, CssBaseline } from '@mui/material'
 import theme from './Themes/theme'
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
 root.render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
+      <CssBaseline />
       <Routes />
     </ThemeProvider>
   </React.StrictMode>
