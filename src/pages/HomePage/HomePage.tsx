0283f782125c91626d5542326c6d5af3460eaa26
Alexandre <eztalenda@gmail.com>
2024-08-04 02:46:01 -0300

    feat: Adjustments to papers to organize the page structure.

--- src/pages/HomePage/HomePage.tsx
+++ src/pages/HomePage/HomePage.tsx
@@ -1,18 +1,19 @@
 import React from 'react'
-import { Typography, Paper, Box, Grid } from '@mui/material'
+import { Typography, Paper, Box, Grid, IconButton, Divider } from '@mui/material'
 import { useTheme } from '@mui/material/styles'
 import { MenuBar } from '../../components'
+import FilterAltIcon from '@mui/icons-material/FilterAlt'
 
 export const HomePage = () => {
   const theme = useTheme()
 
   return (
-    <Box style={{ height: '100vh', backgroundColor: theme.palette.background.default }}>
+    <Box style={{ minHeight: '100vh', backgroundColor: theme.palette.background.default }}>
       <Grid container direction="column">
         <Grid item>
           <MenuBar />
         </Grid>
-        <Grid item xs>
+        <Grid item>
           <Paper
             style={{
               padding: theme.spacing(2),
@@ -20,8 +21,48 @@ export const HomePage = () => {
               backgroundColor: theme.palette.background.paper,
             }}
           >
+            <Box
+              display="flex"
+              alignItems="center"
+              justifyContent="space-between"
+              sx={{ marginBottom: theme.spacing(2) }}
+            >
+              <Typography variant="h3" style={{ color: theme.palette.text.primary, fontWeight: 'bold' }}>
+                Painel de Controle
+              </Typography>
+              <Box display="flex" alignItems="center" gap={1}>
+                <IconButton
+                  aria-label="filter"
+                  sx={{
+                    '&:hover': {
+                      backgroundColor: theme.palette.icon?.main,
+                    },
+                    borderRadius: '20%',
+                    padding: theme.spacing(2),
+                  }}
+                >
+                  <Typography variant="h5" style={{ color: theme.palette.text.primary }}>
+                    Filtros
+                  </Typography>
+                  <FilterAltIcon fontSize="large" style={{ color: theme.palette.text.primary }} />
+                </IconButton>
+              </Box>
+            </Box>
+            <Divider sx={{ width: '50%', bgcolor: 'text.primary', height: 2 }} />{' '}
+          </Paper>
+        </Grid>
+        <Grid item xs>
+          <Paper
+            style={{
+              padding: theme.spacing(2),
+              margin: theme.spacing(2),
+              backgroundColor: theme.palette.background.paper,
+              overflow: 'auto',
+              maxHeight: 'calc(100vh - 170px)',
+            }}
+          >
             <Typography variant="body1" style={{ color: theme.palette.text.primary }}>
-              Bem-vindo à HomePage!
+              Conteúdo do DataGrid aqui
             </Typography>
           </Paper>
         </Grid>
