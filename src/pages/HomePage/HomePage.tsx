842e2614de89dd937024bc36ea259292349d951d
Alexandre <eztalenda@gmail.com>
2024-08-04 00:36:49 -0300

    feat: adds a solid foundation to start work on the homepage

--- src/pages/HomePage/HomePage.tsx
+++ src/pages/HomePage/HomePage.tsx
@@ -1,16 +1,31 @@
 import React from 'react'
-import { ThemeProvider } from '@mui/material/styles'
-import Button from '@mui/material/Button'
-import theme from '../../Themes/theme'
+import { Typography, Paper, Box, Grid } from '@mui/material'
+import { useTheme } from '@mui/material/styles'
+import { MenuBar } from '../../components'
 
-function App() {
+export const HomePage = () => {
+  const theme = useTheme()
+
   return (
-    <ThemeProvider theme={theme}>
-      <Button variant="contained" color="primary">
-        Clique Aqui
-      </Button>
-    </ThemeProvider>
+    <Box style={{ height: '100vh', backgroundColor: theme.palette.background.default }}>
+      <Grid container direction="column">
+        <Grid item>
+          <MenuBar />
+        </Grid>
+        <Grid item xs>
+          <Paper
+            style={{
+              padding: theme.spacing(2),
+              margin: theme.spacing(2),
+              backgroundColor: theme.palette.background.paper,
+            }}
+          >
+            <Typography variant="body1" style={{ color: theme.palette.text.primary }}>
+              Bem-vindo à HomePage!
+            </Typography>
+          </Paper>
+        </Grid>
+      </Grid>
+    </Box>
   )
 }
-
-export default App
