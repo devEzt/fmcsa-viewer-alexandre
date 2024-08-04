111cb4125eb582cea94e593b1daca7c641705cd0
Alexandre <eztalenda@gmail.com>
2024-08-03 21:25:08 -0300

    feat: start first page and folder structure with route

---
+++ src/Routes.tsx
@@ -0,0 +1,16 @@
+// src/Routes.tsx
+import React from 'react'
+import { BrowserRouter, Route, Routes as RouterRoutes } from 'react-router-dom'
+import HomePage from './pages/HomePage/HomePage'
+
+const Routes = () => {
+  return (
+    <BrowserRouter>
+      <RouterRoutes>
+        <Route path="/" element={<HomePage />} />
+      </RouterRoutes>
+    </BrowserRouter>
+  )
+}
+
+export default Routes
---
+++ src/pages/HomePage/HomePage.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import { ThemeProvider } from '@mui/material/styles'
+import Button from '@mui/material/Button'
+import theme from '../../Themes/theme'
+
+function App() {
+  return (
+    <ThemeProvider theme={theme}>
+      <Button variant="contained" color="primary">
+        Clique Aqui
+      </Button>
+    </ThemeProvider>
+  )
+}
+
+export default App
